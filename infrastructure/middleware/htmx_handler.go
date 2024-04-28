6edb86e2bd107f259f29be63bdf7aa0018175eba
Robert Mang <Robert.Mang@softgarden.de>
2024-04-28 09:52:24 +0200

    cleanup

--- infrastructure/middleware/htmx_handler.go
+++ infrastructure/middleware/htmx_handler.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"fmt"
-
 	"github.com/MorrisMorrison/retfig/ui/views"
 	"github.com/MorrisMorrison/retfig/ui/views/events"
 
@@ -13,7 +11,6 @@ func HtmxHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		isHxRequest := c.GetHeader("HX-Request")
 
-		fmt.Println(isHxRequest)
 		if isHxRequest == "" || isHxRequest == "false" {
 			c.HTML(204, "", views.Index(events.CreateEvent()))
 			c.Abort()
