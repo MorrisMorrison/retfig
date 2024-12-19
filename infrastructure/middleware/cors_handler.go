22e7f593d08a022d79097192fff81d5a9083175e
Morris Morrison <mwlltrdev@gmail.com>
2024-12-19 14:28:59 +0100

    remove port from origin string

--- infrastructure/middleware/cors_handler.go
+++ infrastructure/middleware/cors_handler.go
@@ -9,7 +9,7 @@ import (
 
 func CORSHandler(config *config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", config.Protocol + "://" + config.Host + ":" + config.Port)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", config.Protocol + "://" + config.Host)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
